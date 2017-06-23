b7e4549080dc6b6d7c8ea92acb2be04c0efe5cb7
ahang <hang.andy.t@gmail.com>
2017-06-23 11:38:47 -0500

    fixed url on repo on footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="footer navbar-fixed-bottom">
-            <p><a href="/">NYT-ReactSearch</a> | <a href="www.github.com/ahang/nyt-reactsearch">Github</a></p>
+            <p><a href="/">NYT-ReactSearch</a> | <a href="http://www.github.com/ahang/nyt-reactsearch">Github</a></p>
         </div>
     )
 }
