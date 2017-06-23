a5ac24dd1f40b814bab66c5beef5c59fdfbe6987
ahang <hang.andy.t@gmail.com>
2017-06-22 22:05:03 -0500

    trying again

--- app/app.js
+++ app/app.js
@@ -8,17 +8,15 @@ import SearchScreen from "./components/search_screen";
 import Results from "./components/results";
 import SavedArticles from "./components/results";
 
-const App = () {
+const App = () => {
     return (
-        <div>
-            <HashRouter>
-                <Switch>
-                    <Route exact path = "/search" component = {SearchScreen} />
-                    <Route path = "/saved" component = {SavedArticles} />
-                    <Route exact path = "/" component = {Main} />
-                </Switch>
-            </HashRouter>
-        </div>
+        <HashRouter>
+            <Switch>
+                <Route exact path = "/search" component = {SearchScreen} />
+                <Route path = "/saved" component = {SavedArticles} />
+                <Route exact path = "/" component = {Main} />
+            </Switch>
+        </HashRouter>
     )
 }
 
